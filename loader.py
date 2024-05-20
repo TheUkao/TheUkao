0ae0b9f90e96bc0b51b59b9c7be8e971155d46b1
TheUkao <msidorchuk@inbox.ru>
2024-05-20 20:06:01 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,5 +2,5 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import TOKEN
 
-bot = Bot(TOKEN)
+bot = Bot(6850164038:AAG7fKbZXlWlpyaP0LJR1HzhEqgs3V1diE0)
 dp = Dispatcher(bot, storage=MemoryStorage())
